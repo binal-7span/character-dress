36a57893f337be4e8b3b01e2d997a386c0d4d453
Binal Gajjar <binal.gajjar7@gmail.com>
2023-01-06 11:45:15 +0530

    Remove exclude from pint json

--- app/Http/Controllers/Api/V1/CharacterController.php
+++ app/Http/Controllers/Api/V1/CharacterController.php
@@ -2,14 +2,12 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Models\Character;
+use App\Traits\ApiResponser;
+use App\Services\CharacterService;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Character\AttachDressRequest;
 use App\Http\Requests\Character\DetachDressRequest;
-use App\Models\Character;
-use App\Models\CharacterDress;
-use App\Models\Dress;
-use App\Services\CharacterService;
-use App\Traits\ApiResponser;
 
 class CharacterController extends Controller
 {
@@ -24,16 +22,18 @@ class CharacterController extends Controller
 
     public function attachDresses(Character $character, AttachDressRequest $request)
     {
-
-        $this->characterService->attachDresses($character,$request->all());
+        $this->characterService->attachDresses($character, $request->all());
         $data['message'] = __('character.attachDress');
+
         return $this->success($data);
     }
 
     public function detachDresses(Character $character, DetachDressRequest $request)
     {
-        $this->characterService->detachDresses($character,$request->all());
+        $this->characterService->detachDresses($character, $request->all());
         $data['message'] = __('character.detachDress');
+        $start = 'test' . env('APP_PATH');
+
         return $this->success($data);
     }
 }
